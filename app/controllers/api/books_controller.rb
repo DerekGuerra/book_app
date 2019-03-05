847302a5243b4bd4158a56f04082ff5a8f1bd4c2
“DerekE” <d12guerra@gmail.comm>
2019-03-05 14:53:30 -0600

    added create action

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -10,4 +10,11 @@ class Api::BooksController < ApplicationController
     @book = Book.find_by(id: the_id)
     render 'show.json.jbuilder'
   end
+
+  def create
+    @book = Book.new(title: params[:input_title], author: params[:input_author], year_published: params[:input_year_published], genre: params[:input_genre])
+    p @book
+    @book.save
+    render 'show.json.jbuilder'
+  end
 end
